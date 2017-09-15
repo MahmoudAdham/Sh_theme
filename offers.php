b3f2e37008e246327b0ccb97410d6bb38ee41242
MahmoudAdham <30972004+MahmoudAdham@users.noreply.github.com>
2017-09-15 22:49:17 +0300

    page offers

--- offers.php
+++ offers.php
@@ -3,135 +3,106 @@
   get_header(); ?>
 <section class="content">
 
-    <div class="bg-title">
-        <div class="container">
-            <h3 class="title-sc">الفنادق الموصى بها</h3>
-            <div class="p-sc">
-                هنا نص تجريبي هنا نص تجريبي هنا نص تجريبي هنا نص تجريبي هنا نص تجريبي هنا نص تجريبي
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="search-item">
-            <form>
-                <div class="row">
-                    <div class="col-md-2">
-                        <h4>ترتيب الفنادق حسب: </h4>
-                    </div>
-                    <div class="col-md-2">
-                        <select class="form-control custom-select">
-                            <option>الدولة</option>
-                            <option>فلسطين</option>
-                            <option>مصر</option>
-                            <option>السعودية</option>
-                        </select>
-                    </div>
-                    <div class="col-md-2">
-                        <select class="form-control custom-select">
-                            <option>السعر</option>
-                            <option>من أعلى سعر إلى أقل سعر</option>
-                            <option>من أقل سعر إلى أعلى سعر</option>
-                        </select>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="rate">
-                            <i class="fa fa-star" aria-hidden="true"></i>
-                            <i class="fa fa-star" aria-hidden="true"></i>
-                            <i class="fa fa-star" aria-hidden="true"></i>
-                            <i class="fa fa-star" aria-hidden="true"></i>
-                            <i class="fa fa-star" aria-hidden="true"></i>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="switch">
-                            <a href="javascript:;" id="grid" class="">
-                                <i class="fa fa-th" aria-hidden="true"></i>
-                            </a>
-                            <a href="javascript:;" id="list" class="active">
-                                <i class="fa fa-th-list " aria-hidden="true"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <!-- <div class="line"></div> -->
-            </form>
-        </div>
-    </div>
-
+    <div class="top-bg-offer"></div>
     <section class="data">
+        <h1>العروض</h1>
         <div class="container">
-
-            <?php
-            $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
-
-            $custom_args = array(
-                'post_type' => 'offers',
-                'posts_per_page' => 4,
-                'paged' => $paged
-            );
-
-            $custom_query = new WP_Query( $custom_args );
-
-            if ( $custom_query->have_posts() ) {
-
-                while ( $custom_query->have_posts() ) {
-                    $custom_query->the_post(); ?>
-                    <div class="row mr-b-5">
-                        <div class="col-12">
-                            <div class="bg-contant no-padding">
-                                <div class="row no-gutters">
-                                    <div class="col-md-4">
-                                        <?php echo get_the_post_thumbnail(); ?>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="row no-gutters">
-                                            <div class="col-md-9">
-                                                <div class="data-hotel">
-                                                    <h4 class="b-inline">
-                                                        <a href="#"><?php the_title(); ?></a>
-                                                    </h4>
-                                                    <div class="rate b-inline">
-                                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                                        <i class="fa fa-star" aria-hidden="true"></i>
-                                                    </div>
-                                                    <p class="some-details">
-                                                        <?php the_content() ?>
-                                                    </p>
-                                                    <a href="#" class="btn btn-more">المزيد</a>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-3">
-                                                <div class="room_price">
-                                                    <div class="room_price_inner">
-                                                        <span class="room_price_number"><del>$89,00</del> <mark>$50,00</mark>  </span>
-                                                        <small class="upper"> في الليلة </small>
-                                                        <a href="room.html" class="btn button upper btn_book">حجز الآن</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+             <p class="mb-0">
+              <button class="btn btn-info" type="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                عروض الفنادق 
+              </button>
+              <button class="btn btn-info" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+               عروض الطيران 
+              </button>
+            </p>
+            
+            <div class="collapse show" id="collapseExample">
+              <div class="card card-block">
+                    <div class="row">
+                        <div class="col-md-6 ">
+                            <img src="http://placehold.it/400x300" style="max-width: 100%;min-width: 100%;">
                         </div>
+                         <div class="col-md-6">
+                            <h1 class="mt-5">احجز غرفتك مبكرا - ادفع أقل </h1>
+                             <p>قمنا بتصميم عرض لك لتبقى معنا لفترة أطول. احجز 21 يوما مقدما واحصل على خصم 15٪ على أفضل سعر متوفر </p>
+                             <div class="price">
+                                <span class="apd">ابتداء من</span>
+                                <span class="from"> 298</span>
+                                <span class="currency">درهم اماراتى</span>
+                             </div>
+
+                                <div class="btn-group mt-5" data-toggle="buttons">
+                                  <button id="book" class="btn btn-primary active">
+                                    احجز الان 
+                                  </button>
+                                  <button id="moreinfo" class="btn btn-primary">
+                                    اعرف أكثر 
+                                  </button>
+
+                                </div>
+
+                        </div>
+
                     </div>
+                  <div class="row">
+                       
+                         <div class="col-md-6">
+                            <h1 class="mt-5">العرض الشهري الخاص</h1>
+                             <p class="mt-5">قمنا بتصميم عرض لك لتبقى معنا لفترة أطول. احجز 21 يوما مقدما واحصل على خصم 15٪ على أفضل سعر متوفر </p>
+                             <div class="price">
+                                <span class="apd">ابتداء من</span>
+                                <span class="from"> 298</span>
+                                <span class="currency">درهم اماراتى</span>
+                             </div>
 
-                <?php   }
-                if (function_exists(custom_pagination)) {
-                    custom_pagination($custom_query->max_num_pages,"",$paged);
-                }
-                wp_reset_postdata();
-            }
+                                <div class="btn-group mt-5" data-toggle="buttons">
+                                  <button id="book" class="btn btn-primary active">
+                                    احجز الان 
+                                  </button>
+                                  <button id="moreinfo" class="btn btn-primary">
+                                    اعرف أكثر 
+                                  </button>
 
-            else {
-                echo "Sorry No Post";
-            }
 
-    ?>
+                                </div>
 
+                        </div>
+                       <div class="col-md-6 ">
+                            <img src="http://placehold.it/400x300" style="max-width: 100%;min-width: 100%;">
+                        </div>
+
+                    </div>
+                   <div class="row">
+                        <div class="col-md-6 ">
+                            <img src="http://placehold.it/400x300" style="max-width: 100%;min-width: 100%;">
+                        </div>
+                         <div class="col-md-6 float-left">
+                            <h1 class="mt-5">اقامة أطول... سعرأقل</h1>
+                             <p>قمنا بتصميم عرض لك لتبقى معنا لفترة أطول. احجز 21 يوما مقدما واحصل على خصم 15٪ على أفضل سعر متوفر </p>
+                             <div class="price">
+                                <span class="apd">ابتداء من</span>
+                                <span class="from"> 298</span>
+                                <span class="currency">درهم اماراتى</span>
+                             </div>
+
+                                <div class="btn-group mt-5" data-toggle="buttons">
+                                 <button id="book" class="btn btn-primary active">
+                                    احجز الان 
+                                  </button>
+                                  <button id="moreinfo" class="btn btn-primary">
+                                    اعرف أكثر 
+                                  </button>
+
+
+                                </div>
+
+                        </div>
+
+                    </div>
+              </div>
+            </div>
+        
+          
         </div>
     </section>
 
